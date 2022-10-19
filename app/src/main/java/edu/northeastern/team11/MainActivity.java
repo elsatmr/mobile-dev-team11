ea93f9d16765ab24efd6e8f79d4530325cdf78f9
elsatmr <elsaatamara@gmail.com>
2022-10-18 17:47:10 -0700

    created main activity ui, at your service button opens up an empty at your service activity

--- app/src/main/java/edu/northeastern/team11/MainActivity.java
+++ app/src/main/java/edu/northeastern/team11/MainActivity.java
@@ -2,7 +2,11 @@ package edu.northeastern.team11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void openAtYourServiceActivity(View view) {
+        Intent intent = new Intent(this, AtYourServiceActivity.class);
+        startActivity(intent);
+    }
+
 }
