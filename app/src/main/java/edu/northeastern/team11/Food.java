47dc1753a46dc1019b654333d5481df0b6e83d5d
Ashley Cheung <ashleycheung@Ashleys-MacBook-Air.local>
2022-10-21 13:06:22 -0700

    adding depency for image loading from url

--- app/src/main/java/edu/northeastern/team11/Food.java
+++ app/src/main/java/edu/northeastern/team11/Food.java
@@ -13,11 +13,23 @@ public class Food {
     @SerializedName("body")
     private String text;
 
+    public Food(String meals, String text) {
+        this.meals = meals;
+        this.text = editBody();
+    }
+
     public String getMeals() {
         return meals;
     }
 
     public String getText() {
         return text;
+    }
+
+    private String editBody() {
+
+
+
+        return "";
     }
 }
