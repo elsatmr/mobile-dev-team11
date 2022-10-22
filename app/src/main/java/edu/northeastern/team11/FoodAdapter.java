45350123e00ba6ba5605dde2791fed43d465cdd9
tomiris <kozhabekkyzy.t@northeastern.edu>
2022-10-22 14:46:36 -0400

    Testing with glide and picasso

--- app/src/main/java/edu/northeastern/team11/FoodAdapter.java
+++ app/src/main/java/edu/northeastern/team11/FoodAdapter.java
@@ -2,20 +2,27 @@ package edu.northeastern.team11;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
+import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
 
@@ -45,7 +52,7 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodViewHolder> {
 
         //Picasso.get().load(imageUri).resize(126, 126)
 
-        Glide.with(context).load(imageUri).override(126, 126).into(holder.foodPhoto);
+        //Glide.with(context).load(imageUri).override(126, 126).into(holder.foodPhoto);
 
 //        final OkHttpClient client = new OkHttpClient.Builder()
 //                .protocols(Collections.singletonList(Protocol.HTTP_1_1))
@@ -57,7 +64,9 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodViewHolder> {
 //
 //        Picasso.setSingletonInstance(picasso);
 //
-//        Picasso.get().load(imageUri).into(holder.foodPhoto, new Callback() {
+        //"https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg"
+
+//        Picasso.get().load(imageUri).resize(126, 126).into(holder.foodPhoto, new Callback() {
 //            @Override
 //            public void onSuccess() {
 //                Log.d("PICASSO", "Success");
@@ -73,18 +82,32 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodViewHolder> {
 //                .centerCrop()
 //                .placeholder(R.mipmap.ic_launcher_round)
 //                .error(R.mipmap.ic_launcher_round);
-
-        //Glide.with(context).load(imageUri).apply(options).into(holder.foodPhoto);
+//
+//        Glide.with(context).load(imageUri).apply(options).into(holder.foodPhoto);
 //        Glide.with(context)
 //                .asBitmap()
 //                .load(imageUri)
 //                .into(holder.foodPhoto);
+//
+        Glide.with(context)
+                .load("https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg")
+                .override(126, Target.SIZE_ORIGINAL)
+                .fitCenter()
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        // log exception
+                        Log.e("TAG", "Error loading image", e);
+                        return false; // important to return false so the error placeholder can be placed
+                    }
 
-//        Glide.with(context)
-//                .load(imageUri)
-//                .override(126, 126)
-//                .centerCrop()
-//                .into(holder.foodPhoto);
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        return false;
+                    }
+                })
+                .into(holder.foodPhoto);
+
     }
 
     @Override
