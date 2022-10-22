1ed8f6303f0819035a129446e53ef1aaa97450a5
Mhart400 <mhart400@gmail.com>
2022-10-22 09:30:05 -0400

    Before removing working files

--- app/src/main/java/edu/northeastern/team11/AtYourServiceActivity.java
+++ app/src/main/java/edu/northeastern/team11/AtYourServiceActivity.java
@@ -3,6 +3,7 @@ package edu.northeastern.team11;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
@@ -15,23 +16,11 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.content.ContextCompat;
 
@@ -43,21 +32,9 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.google.android.material.chip.Chip;
-import com.google.android.material.chip.ChipGroup;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-
-import org.json.JSONArray;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -87,11 +64,14 @@ public class AtYourServiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_at_your_service);
-        addSearchButtonListener();
+
         result_tv = findViewById(R.id.results);
         search_button = findViewById(R.id.search_button);
         searchBar = findViewById(R.id.search_bar);
         foodList = new ArrayList<>();
+        addSearchButtonListener();
+
+        textInputContents = "";
         searchList = new ArrayList<String>();
         searchInput = findViewById(R.id.searchInput);
         clearButton = findViewById(R.id.clearTextBtn);
@@ -106,8 +86,153 @@ public class AtYourServiceActivity extends AppCompatActivity {
 
     }
 
+    // When the user types in EditText store the string
+    // When the user deletes all test in the search input, add the hint back
+    private void handleTyping() {
+        searchInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
 
-    // Set the
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                textInputContents = charSequence.toString().trim();
+                if (textInputContents.length() > 0) {
+                    clearButton.setVisibility(View.VISIBLE);
+                    searchButton.setEnabled(true);
+                } else {
+                    clearButton.setVisibility(View.INVISIBLE);
+                    searchButton.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+        searchInput.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if (keyEvent.getAction() == KeyEvent.KEYCODE_ENTER) {
+                    addChip();
+                }
+                return false;
+            }
+        });
+    }
+
+    // Clear the search EditText
+    public void clearSearchInput(View view) {
+        searchInput.setText("");
+        textInputContents = "";
+        searchInput.requestFocus();
+    }
+
+    // Add Chipgroup to hold the chips
+    private void addChipGroup() {
+        chipGroup = new ChipGroup(this);
+        chipGroup.setId(R.id.chipGroup);
+        constLayout.addView(chipGroup);
+        ConstraintSet chipGroupConstraint = new ConstraintSet();
+        chipGroupConstraint.constrainHeight(chipGroup.getId(), ConstraintSet.WRAP_CONTENT);
+        chipGroupConstraint.constrainWidth(chipGroup.getId(), ConstraintSet.WRAP_CONTENT);
+        chipGroupConstraint.connect(chipGroup.getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT);
+        chipGroupConstraint.connect(chipGroup.getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT);
+        chipGroupConstraint.connect(chipGroup.getId(), ConstraintSet.TOP, toolbar.getId(), ConstraintSet.BOTTOM);
+        chipGroupConstraint.applyTo(constLayout);
+    }
+
+    // Add a chip
+    public void addChip(View view) {
+        if (textInputContents.length() > 0) {
+            addChip(textInputContents);
+        }
+    }
+
+    // Add a chip
+    private void addChip() {
+        if (textInputContents.length() > 0) {
+            addChip(textInputContents);
+        }
+    }
+
+    // Add a chip
+    // THIS SHOULD TRIGGER THE GET REQUEST
+    private void addChip(String searchString) {
+        if (chipGroup == null) {
+            addChipGroup();
+        }
+        // Create the chip and give it an ID
+        Chip newChip = new Chip(this);
+        newChip.setId(chipID);
+//        chipIDs.add(chipID);
+        chipID++;
+        searchList.add(searchString);
+        newChip.setText(searchString);
+        newChip.setTextColor(Color.parseColor("#FAF8F8"));
+        newChip.setChipBackgroundColor(ColorStateList.valueOf(Color.parseColor("#19444C")));
+        clearSearchInput(null);
+
+        // Set the onClose listener for each chip
+        newChip.setCloseIcon(ContextCompat.getDrawable(this, android.R.drawable.ic_menu_close_clear_cancel));
+        newChip.setCloseIconTint(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
+        newChip.setCloseIconVisible(true);
+        newChip.setOnCloseIconClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chipGroup.removeView(newChip);
+                searchList.remove(newChip.getText());
+                chipList.remove(newChip);
+
+            }
+        });
+        chipList.add(newChip);
+        chipGroup.addView(newChip);
+        closeKeyboard();
+        searchInput.clearFocus();
+    }
+
+    // Close the keyboard
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            manager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("chipID", chipID);
+//        outState.putSerializable("chipIDs", (Serializable) chipIDs);
+        outState.putSerializable("searchList", (Serializable) searchList);
+        outState.putSerializable("chipList", (Serializable) chipList);
+        chipGroup.removeAllViews();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        chipID = savedInstanceState.getInt("chipID");
+//        chipIDs = (List<Integer>) savedInstanceState.getSerializable("chipIDs");
+        searchList = (List<String>) savedInstanceState.getSerializable("searchList");
+        chipList = (List<Chip>) savedInstanceState.getSerializable("chipList");
+        addChipGroup();
+        chipList.forEach(x -> {
+            chipGroup.addView(x);
+            x.setOnCloseIconClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    chipGroup.removeView(x);
+                    searchList.remove(x.getText());
+                    chipList.remove(x);
+                }
+            });
+        });
+    }
+
+    // Set the listener on the search button so that we get a list of foods
     private void addSearchButtonListener() {
         search_button.setOnClickListener(new View.OnClickListener() {
             @Override
