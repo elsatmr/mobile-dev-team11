2a62ae779bc654522e1d15974eb7648675dea63b
Mhart400 <mhart400@gmail.com>
2022-11-03 15:28:20 -0400

    Added username validation for signup and login

--- app/src/main/java/edu/northeastern/team11/AllStickersActivity.java
+++ app/src/main/java/edu/northeastern/team11/AllStickersActivity.java
@@ -1,13 +1,30 @@
 package edu.northeastern.team11;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AllStickersActivity extends AppCompatActivity {
+    private TextView currentUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_stickers_screen);
+
+        // PLACEHOLDER - This is how you get the current user from Shared Preferences
+        currentUser = findViewById(R.id.currentUser);
+        currentUser.setText("My username is " + getCurrentUser());
+
+    }
+
+
+    // get the username of who is signed in
+    private String getCurrentUser() {
+        SharedPreferences sharedPref = getSharedPreferences("settings",0);
+        return sharedPref.getString("username", null);
     }
 }
--- app/src/main/java/edu/northeastern/team11/SignUpActivity.java
+++ app/src/main/java/edu/northeastern/team11/SignUpActivity.java
@@ -1,9 +1,15 @@
 package edu.northeastern.team11;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -17,38 +23,65 @@ import com.google.firebase.database.ValueEventListener;
 
 public class SignUpActivity extends AppCompatActivity {
     private FirebaseDatabase mDatabase;
-
-
-
+    private EditText newUser;
+    private Button signUpButton;
+    private TextView errorText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        final EditText newUser = findViewById(R.id.new_username);
-        String username = newUser.getText().toString();
-        final Button signUpButton = findViewById(R.id.signup_new_user);
+        // Get UI element references
+        newUser = findViewById(R.id.new_username);
+        signUpButton = findViewById(R.id.signup_new_user);
+        errorText = findViewById(R.id.errorText);
 
         // connect with firebase
         mDatabase = FirebaseDatabase.getInstance();
+        errorText.setVisibility(View.INVISIBLE);
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DatabaseReference ref = mDatabase.getReferenceFromUrl("https://stickers-19c0f-default-rtdb.firebaseio.com/");
+                if (newUser.getText().toString().trim().length() > 0) {
+                    handleSignup();
+                }
+            }
+        });
+
+    }
+
+    private void handleSignup() {
+        DatabaseReference db = mDatabase.getReference();
+        String queryString = "users/" + newUser.getText().toString().trim();
+        db.child(queryString).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) { // username already exists
+                    errorText.setVisibility(View.VISIBLE);
+                    newUser.setText("");
+                    newUser.requestFocus();
+                } else { // username is available
+                    // Write username to database
+
+                    // Save username in shared preferences
+                    SharedPreferences.Editor editor = getSharedPreferences("settings", Context.MODE_PRIVATE).edit();
+                    editor.putString("username", newUser.getText().toString().trim());
+                    editor.apply();
+                    // Go to All Stickers Page
+                    goToAllStickersScreen();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
 
-
-
-
-
-
-
-
-
-
-
-
+    private void goToAllStickersScreen() {
+        Intent intent = new Intent(this, AllStickersActivity.class);
+        startActivity(intent);
+    }
 }
