0f441697535a2e41b0a29f77400736700bcd4818
Ashley <cheung.as@northeastern.edu>
2022-11-04 14:24:03 -0700

    making users clickable, adding done button

--- app/src/main/java/edu/northeastern/team11/SendStickerActivity.java
+++ app/src/main/java/edu/northeastern/team11/SendStickerActivity.java
@@ -1,14 +1,20 @@
 package edu.northeastern.team11;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,6 +27,7 @@ public class SendStickerActivity extends AppCompatActivity {
 
     private TextView header;
     private ListView listView;
+    private ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,7 @@ public class SendStickerActivity extends AppCompatActivity {
         // get UI elements
         header = findViewById(R.id.send_screen_instructions);
         listView = findViewById(R.id.list_view);
+        constraintLayout = findViewById(R.id.layout_send_sticker);
 
         // set up listView adapter and arraylist to add to adapter
         ArrayList<String> usersList = new ArrayList<>();
@@ -44,7 +52,6 @@ public class SendStickerActivity extends AppCompatActivity {
                 usersList.clear();
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     usersList.add(snapshot.getKey());
-
                 }
                 adapter.notifyDataSetChanged();
 
@@ -55,9 +62,31 @@ public class SendStickerActivity extends AppCompatActivity {
 
             }
         });
+        sendSticker();
 
 
 
 
     }
+
+    private void sendSticker() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // create a new transaction
+                // update sentCount from sender
+                // update receivedCount from receiver
+//                Snackbar snackbar = Snackbar.make(constraintLayout, "Sticker sent!", Snackbar.LENGTH_LONG);
+//                snackbar.show();
+
+            }
+        });
+    }
+
+    // when finished sending stickers, go back to the all stickers screen
+    public void doneButtonPressed(View view) {
+        Intent intent = new Intent(this, AllStickersActivity.class);
+        startActivity(intent);
+    }
+
 }
