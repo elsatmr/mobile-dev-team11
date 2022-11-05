00a3acb8ab1f0be92522b0cd9ea546b3fabbf375
Ashley <cheung.as@northeastern.edu>
2022-11-05 12:33:28 -0700

    updating user send/receive counts

--- app/src/main/java/edu/northeastern/team11/SendStickerActivity.java
+++ app/src/main/java/edu/northeastern/team11/SendStickerActivity.java
@@ -21,12 +21,17 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class SendStickerActivity extends AppCompatActivity {
@@ -81,7 +86,7 @@ public class SendStickerActivity extends AppCompatActivity {
         });
     }
 
-    // get index of new entry, then write the new transaction to the db
+    // generate a uniqueId for the key, then write the new transaction to the db
     private void addNewTransactionToDb(String receiver) {
 
             DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
@@ -100,6 +105,29 @@ public class SendStickerActivity extends AppCompatActivity {
             String key = ref.child("transactions").push().getKey();
             // write to database
             ref.child("transactions").child(key).setValue(transaction);
+
+            // after sending sticker, set the listview to invisible
+            // add snackbar - sticker sent!
+            // change done button to "send Sticker" which will cal updateSentReccievedCounts
+            // and return to all stickers activity
+
+            updateSentReceivedCounts(receiver, sender);
+    }
+
+    // update user's receivedCount snd sentCount
+    private void updateSentReceivedCounts(String receiver, String sender) {
+        // need to get the stickerId, for now just hardcoding 0
+        String stickerId = String.valueOf(0);
+
+        // get db ref
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
+        Map<String, Object> updates = new HashMap<>();
+        // update senderCount
+        updates.put("users/"+sender+"/"+stickerId+"/sentCount", ServerValue.increment(1));
+        updates.put("users/"+receiver+"/"+stickerId+"/receivedCount", ServerValue.increment(1));
+        ref.updateChildren(updates);
+        // setValue()
     }
 
 
@@ -139,5 +167,105 @@ public class SendStickerActivity extends AppCompatActivity {
             return stickerId;
         }
     }
+
+    private class User {
+        private StickerHistory s0;
+        private StickerHistory s1;
+        private StickerHistory s2;
+        private StickerHistory s3;
+        private StickerHistory s4;
+        private StickerHistory s5;
+        private StickerHistory s6;
+        private StickerHistory s7;
+        private StickerHistory s8;
+        private StickerHistory s9;
+
+        public User(StickerHistory s0, StickerHistory s1, StickerHistory s2, StickerHistory s3,
+                    StickerHistory s4, StickerHistory s5, StickerHistory s6, StickerHistory s7,
+                    StickerHistory s8, StickerHistory s9) {
+            this.s0 = s0;
+            this.s1 = s1;
+            this.s2 = s2;
+            this.s3 = s3;
+            this.s4 = s4;
+            this.s5 = s5;
+            this.s6 = s6;
+            this.s7 = s7;
+            this.s8 = s8;
+            this.s9 = s9;
+        }
+
+        public StickerHistory getS0() {
+            return s0;
+        }
+
+        public StickerHistory getS1() {
+            return s1;
+        }
+
+        public StickerHistory getS2() {
+            return s2;
+        }
+
+        public StickerHistory getS3() {
+            return s3;
+        }
+
+        public StickerHistory getS4() {
+            return s4;
+        }
+
+        public StickerHistory getS5() {
+            return s5;
+        }
+
+        public StickerHistory getS6() {
+            return s6;
+        }
+
+        public StickerHistory getS7() {
+            return s7;
+        }
+
+        public StickerHistory getS8() {
+            return s8;
+        }
+
+        public StickerHistory getS9() {
+            return s9;
+        }
+    }
+
+    private class StickerHistory {
+        private int receivedCount;
+        private int sentCount;
+        private String urlString;
+
+        public StickerHistory(int receivedCount, int sentCount, String urlString) {
+            this.receivedCount = receivedCount;
+            this.sentCount = sentCount;
+            this.urlString = urlString;
+        }
+        public StickerHistory() {
+            this.receivedCount = receivedCount;
+            this.sentCount = sentCount;
+            this.urlString = urlString;
+        }
+
+        public int getReceivedCount() {
+            return receivedCount;
+        }
+
+        public int getSentCount() {
+            return sentCount;
+        }
+
+        public String getUrlString() {
+            return urlString;
+        }
+    }
+
+
+
 
 }
