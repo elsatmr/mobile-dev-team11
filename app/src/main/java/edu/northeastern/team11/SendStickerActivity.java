5a725e4ebf0944cca4a6f412742d54d2f10b4771
Ashley <cheung.as@northeastern.edu>
2022-11-05 12:38:17 -0700

    cleanup

--- app/src/main/java/edu/northeastern/team11/SendStickerActivity.java
+++ app/src/main/java/edu/northeastern/team11/SendStickerActivity.java
@@ -106,11 +106,6 @@ public class SendStickerActivity extends AppCompatActivity {
             // write to database
             ref.child("transactions").child(key).setValue(transaction);
 
-            // after sending sticker, set the listview to invisible
-            // add snackbar - sticker sent!
-            // change done button to "send Sticker" which will cal updateSentReccievedCounts
-            // and return to all stickers activity
-
             updateSentReceivedCounts(receiver, sender);
     }
 
@@ -123,18 +118,10 @@ public class SendStickerActivity extends AppCompatActivity {
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
 
         Map<String, Object> updates = new HashMap<>();
-        // update senderCount
+        // update senderCount and receiverCount
         updates.put("users/"+sender+"/"+stickerId+"/sentCount", ServerValue.increment(1));
         updates.put("users/"+receiver+"/"+stickerId+"/receivedCount", ServerValue.increment(1));
         ref.updateChildren(updates);
-        // setValue()
-    }
-
-
-    // when finished sending stickers, go back to the all stickers screen
-    public void doneButtonPressed(View view) {
-        Intent intent = new Intent(this, AllStickersActivity.class);
-        startActivity(intent);
     }
 
     private class Transaction {
@@ -168,73 +155,73 @@ public class SendStickerActivity extends AppCompatActivity {
         }
     }
 
-    private class User {
-        private StickerHistory s0;
-        private StickerHistory s1;
-        private StickerHistory s2;
-        private StickerHistory s3;
-        private StickerHistory s4;
-        private StickerHistory s5;
-        private StickerHistory s6;
-        private StickerHistory s7;
-        private StickerHistory s8;
-        private StickerHistory s9;
-
-        public User(StickerHistory s0, StickerHistory s1, StickerHistory s2, StickerHistory s3,
-                    StickerHistory s4, StickerHistory s5, StickerHistory s6, StickerHistory s7,
-                    StickerHistory s8, StickerHistory s9) {
-            this.s0 = s0;
-            this.s1 = s1;
-            this.s2 = s2;
-            this.s3 = s3;
-            this.s4 = s4;
-            this.s5 = s5;
-            this.s6 = s6;
-            this.s7 = s7;
-            this.s8 = s8;
-            this.s9 = s9;
-        }
-
-        public StickerHistory getS0() {
-            return s0;
-        }
-
-        public StickerHistory getS1() {
-            return s1;
-        }
-
-        public StickerHistory getS2() {
-            return s2;
-        }
-
-        public StickerHistory getS3() {
-            return s3;
-        }
-
-        public StickerHistory getS4() {
-            return s4;
-        }
-
-        public StickerHistory getS5() {
-            return s5;
-        }
-
-        public StickerHistory getS6() {
-            return s6;
-        }
-
-        public StickerHistory getS7() {
-            return s7;
-        }
-
-        public StickerHistory getS8() {
-            return s8;
-        }
-
-        public StickerHistory getS9() {
-            return s9;
-        }
-    }
+//    private class User {
+//        private StickerHistory s0;
+//        private StickerHistory s1;
+//        private StickerHistory s2;
+//        private StickerHistory s3;
+//        private StickerHistory s4;
+//        private StickerHistory s5;
+//        private StickerHistory s6;
+//        private StickerHistory s7;
+//        private StickerHistory s8;
+//        private StickerHistory s9;
+//
+//        public User(StickerHistory s0, StickerHistory s1, StickerHistory s2, StickerHistory s3,
+//                    StickerHistory s4, StickerHistory s5, StickerHistory s6, StickerHistory s7,
+//                    StickerHistory s8, StickerHistory s9) {
+//            this.s0 = s0;
+//            this.s1 = s1;
+//            this.s2 = s2;
+//            this.s3 = s3;
+//            this.s4 = s4;
+//            this.s5 = s5;
+//            this.s6 = s6;
+//            this.s7 = s7;
+//            this.s8 = s8;
+//            this.s9 = s9;
+//        }
+//
+//        public StickerHistory getS0() {
+//            return s0;
+//        }
+//
+//        public StickerHistory getS1() {
+//            return s1;
+//        }
+//
+//        public StickerHistory getS2() {
+//            return s2;
+//        }
+//
+//        public StickerHistory getS3() {
+//            return s3;
+//        }
+//
+//        public StickerHistory getS4() {
+//            return s4;
+//        }
+//
+//        public StickerHistory getS5() {
+//            return s5;
+//        }
+//
+//        public StickerHistory getS6() {
+//            return s6;
+//        }
+//
+//        public StickerHistory getS7() {
+//            return s7;
+//        }
+//
+//        public StickerHistory getS8() {
+//            return s8;
+//        }
+//
+//        public StickerHistory getS9() {
+//            return s9;
+//        }
+//    }
 
     private class StickerHistory {
         private int receivedCount;
@@ -264,8 +251,4 @@ public class SendStickerActivity extends AppCompatActivity {
             return urlString;
         }
     }
-
-
-
-
 }
