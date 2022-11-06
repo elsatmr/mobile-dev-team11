120d83d566a38e32e47de67d3f89ed3d4a6da704
tomiris <kozhabekkyzy.t@northeastern.edu>
2022-11-06 13:57:47 -0500

    Fixed intent issue

--- app/src/main/java/edu/northeastern/team11/AllStickersActivity.java
+++ app/src/main/java/edu/northeastern/team11/AllStickersActivity.java
@@ -17,6 +17,20 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+
+import android.os.Build;
+import androidx.annotation.Nullable;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import com.google.firebase.database.ChildEventListener;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -34,6 +48,10 @@ public class AllStickersActivity extends AppCompatActivity {
     private FirebaseDatabase firebaseDb;
     private List<Sticker> stickerList;
     StickerAdapter adapter;
+    private DatabaseReference transactionRef;
+    NotificationManagerCompat notificationManagerCompat;
+    Notification notification;
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +59,14 @@ public class AllStickersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_all_stickers_screen);
         firebaseDb = FirebaseDatabase.getInstance();
         dbRef = firebaseDb.getReferenceFromUrl("https://stickers-19c0f-default-rtdb.firebaseio.com/");
-        String username = this.getCurrentUser();
+
+        username = this.getCurrentUser();
+        transactionRef = firebaseDb.getReference().child("transactions");
+
         Query userQuery = dbRef.child("users").child(username);
         stickerList = new ArrayList<>();
         Log.d("QUERY", userQuery.toString());
+        createNotificationChannel();
         userQuery.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -62,6 +84,40 @@ public class AllStickersActivity extends AppCompatActivity {
 
             }
         });
+
+        transactionRef.orderByKey().limitToLast(1).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                //Log.d("DATA SNAPSHOT T", snapshot.getKey());
+                String receiver = snapshot.child("receiver").getValue(String.class);
+                String sender = snapshot.child("sender").getValue(String.class);
+                String stickerId = snapshot.child("stickerId").getValue(String.class);
+
+                Log.d("RECEIVER", receiver);
+                Log.d("SENDER", sender);
+                Log.d("RECEIVED Sticker ID", stickerId);
+
+                if (Objects.equals(username, receiver)) {
+                    sendNotification(sender, stickerId);
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
         RecyclerView stickerRecyclerView = findViewById(R.id.sticker_recycler_view);
         stickerRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
         adapter = new StickerAdapter(stickerList, this);
@@ -73,6 +129,65 @@ public class AllStickersActivity extends AppCompatActivity {
     private String getCurrentUser() {
         SharedPreferences sharedPref = getSharedPreferences("settings", 0);
         return sharedPref.getString("username", null);
+    }
+
+    public void createNotificationChannel() {
+        // This must be called early because it must be called before a notification is sent.
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Received sticker notification";
+            String description = "Notification channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("Channel_Sticker_ID", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void sendNotification(String senderName, String stickerId) {
+
+        // Prepare intent which is triggered if the
+        // notification is selected
+        Intent intent = new Intent(this, StickerScreen.class);
+        intent.putExtra("stickerId", stickerId);
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, PendingIntent.FLAG_MUTABLE);
+
+        PendingIntent callIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(),
+                new Intent(this, StickerScreen.class), PendingIntent.FLAG_MUTABLE);
+
+        // Build notification
+        // Need to define a channel ID after Android Oreo
+        String channelId = "Channel_Sticker_ID";
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "Channel_Sticker_ID")
+                .setSmallIcon(android.R.drawable.stat_notify_chat)
+                .setContentTitle("New sticker from " + senderName)
+                .setContentText("You received new sticker!")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pIntent)
+                // hide the notification after its selected
+                .setAutoCancel(true);
+
+        notification = builder.build();
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+
+//        NotificationCompat.Builder notifyBuild = new NotificationCompat.Builder(this, channelId)
+//                //"Notification icons must be entirely white."
+//                .setSmallIcon(R.drawable.foo)
+//                .setContentTitle("New mail from " + "test@test.com")
+//                .setContentText("Subject")
+//                .setPriority(NotificationCompat.PRIORITY_HIGH)
+//                // hide the notification after its selected
+//                .setAutoCancel(true)
+//                .addAction(R.drawable.foo, "Call", callIntent)
+//                .setContentIntent(pIntent);
+
+        // // notificationId is a unique int for each notification that you must define
+        notificationManagerCompat.notify(0, notification);
     }
 
 }
