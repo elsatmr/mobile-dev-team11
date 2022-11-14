e01d652b6ee37eb36b569745bca15fc3f9b627a8
Mhart400 <mhart400@gmail.com>
2022-11-14 11:18:35 -0500

    Added Slurp landing screen

--- app/src/main/java/edu/northeastern/team11/MainActivity.java
+++ app/src/main/java/edu/northeastern/team11/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import edu.northeastern.team11.slurp.LandingScreen;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void openStickersActivity(View view) {
         Intent intent = new Intent(this, StickersMainActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToSlurp(View view) {
+        Intent intent = new Intent(this, LandingScreen.class);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/edu/northeastern/team11/slurp/LandingScreen.java
@@ -0,0 +1,16 @@
+package edu.northeastern.team11.slurp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.northeastern.team11.R;
+
+public class LandingScreen extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.slurp_landing);
+    }
+
+}
