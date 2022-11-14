39ef15a7cf2dbde89cc7594f022044ce87ca76e0
Mhart400 <mhart400@gmail.com>
2022-11-14 16:35:22 -0500

    Added MapBox functionality to app on the Search Dishes page

--- app/src/main/java/edu/northeastern/team11/slurp/SearchDishesScreen.java
+++ app/src/main/java/edu/northeastern/team11/slurp/SearchDishesScreen.java
@@ -2,13 +2,19 @@ package edu.northeastern.team11.slurp;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
 
 import edu.northeastern.team11.R;
 
 public class SearchDishesScreen extends AppCompatActivity {
+
+    MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +24,14 @@ public class SearchDishesScreen extends AppCompatActivity {
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.search_dishes);
         bottomNavigationView.setOnItemSelectedListener(new BottomTabListener(this));
+
+        // Hide top action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        // MapBox
+        mapView = findViewById(R.id.mapView);
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
     }
 
 }
