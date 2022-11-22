efd3dece78a570f88e949bac2d05debfca980863
Ashley <cheung.as@northeastern.edu>
2022-11-22 13:08:55 -0800

    identifies if user searched exists or not

--- app/src/main/java/edu/northeastern/team11/slurp/SearchUserFragment.java
+++ app/src/main/java/edu/northeastern/team11/slurp/SearchUserFragment.java
@@ -4,12 +4,25 @@ import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import edu.northeastern.team11.R;
 
@@ -24,6 +37,13 @@ public class SearchUserFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    DatabaseReference db;
+    EditText userSearched;
+    TextView errMsg;
+    Button searchButton;
+    CardView cardView;
+    String curUser;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -66,12 +86,48 @@ public class SearchUserFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.slurp_fragment_search_user, container, false);
-        String userName = getCurUserSearchUserFrag();
+        curUser = getCurUserSearchUserFrag();
 //        TextView tv = (TextView) view.findViewById(R.id.searchUser_frag_user);
 //        tv.setText("SEARCH USER FRAG, Current User: " + userName);
 
+        userSearched = view.findViewById(R.id.search_username_et);
+        searchButton = view.findViewById(R.id.search_for_user_button);
+        cardView = view.findViewById(R.id.user_cardView);
+        errMsg = view.findViewById(R.id.search_user_error_msg);
+        errMsg.setVisibility(View.INVISIBLE);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findUser();
+            }
+        });
+
         return view;
     }
+
+    private void findUser() {
+        db = FirebaseDatabase.getInstance().getReference();
+
+        db.child("users_slurp").child(userSearched.getText().toString()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (userSearched.getText().toString().length()!= 0) {
+                    if (task.isSuccessful() && task.getResult().getValue() != null) {
+                        // username exists
+                        Log.d("here", String.valueOf(task.getResult().getKey()));
+                    } else {
+                        // searched username does not exist
+                        Log.d("error getting data: ", String.valueOf(task.getException()));
+                    }
+                } else {
+                    Log.d("error","EditText username is empty" );
+                }
+
+                }
+        });
+    }
+
 
     // get the current user from shared preferences
     private String getCurUserSearchUserFrag() {
