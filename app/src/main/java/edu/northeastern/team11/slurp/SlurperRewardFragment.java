fa8220f335142f8df0fe80ab30762f05d3a22a83
elsatmr <elsaatamara@gmail.com>
2022-11-29 23:41:24 -0800

    my friends tab layout and slurper status done

--- app/src/main/java/edu/northeastern/team11/slurp/SlurperRewardFragment.java
+++ app/src/main/java/edu/northeastern/team11/slurp/SlurperRewardFragment.java
@@ -1,12 +1,23 @@
 package edu.northeastern.team11.slurp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import edu.northeastern.team11.R;
 
@@ -60,7 +71,54 @@ public class SlurperRewardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_slurper_reward, container, false);
+        View view = inflater.inflate(R.layout.fragment_slurper_reward, container, false);
+        String username = this.getCurUserProfileFrag();
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://stickers-19c0f-default-rtdb.firebaseio.com/");
+        dbRef.child("users_slurp").child(username).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                setSlurperStatus(user, view);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return view;
+    }
+
+    private String getCurUserProfileFrag() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("settings", 0);
+        return sharedPreferences.getString("username", null);
+    }
+
+    private void setSlurperStatus(User user, View view) {
+        long number = user.getSlurperStatusPoints();
+        TextView slurperStatusTextView = view.findViewById(R.id.slurperStatusTextView);
+        TextView slurperNumFriendsTV = view.findViewById(R.id.numFriendsTextView);
+        TextView slurperNumVotesTV = view.findViewById(R.id.numVotesTextView);
+        TextView slurperNumPostsTV = view.findViewById(R.id.numPostsTextView);
+        ImageView slurperStatusImageView = view.findViewById(R.id.slurperStatusEmblem);
+        if (number < 5) {
+            slurperStatusTextView.setText("Baby Slurper \uD83C\uDF7C");
+            slurperStatusImageView.setImageResource(R.drawable.baby);
+        } else if (number >= 5 && number < 10) {
+            slurperStatusTextView.setText("Slurper Jr. ✨");
+            slurperStatusImageView.setImageResource(R.drawable.junior);
+        } else if (number >= 10 && number < 20) {
+            slurperStatusTextView.setText("Slurper Sr. \uD83D\uDCAA");
+            slurperStatusImageView.setImageResource(R.drawable.senior);
+        } else if (number >= 20 && number < 100) {
+            slurperStatusTextView.setText("Slurper Elite Force \uD83D\uDC6E");
+            slurperStatusImageView.setImageResource(R.drawable.elite);
+        } else {
+            slurperStatusTextView.setText("Chief of Slurper \uD83E\uDDB9");
+            slurperStatusImageView.setImageResource(R.drawable.chief);
+        }
+        slurperNumFriendsTV.setText("Number of friends: " + String.valueOf(user.getNumFriends()) + " \uD83D\uDE0E");
+        slurperNumPostsTV.setText("Number of posts: " + String.valueOf(user.getNumPosts()) + " \uD83E\uDD73");
+        slurperNumVotesTV.setText("Number of votes: " + String.valueOf(user.getNumTimesVoted()) + " ❤️");
     }
 }
